11e766b61066f64bd1cf62131b04056b5d9b2365
Aneeb <aneebpaloth74@gmail.com>
2023-09-25 10:19:13 -0700

    added cart db

--- base/models.py
+++ base/models.py
@@ -106,3 +106,19 @@ class Product(models.Model):
         return reverse('product_details',args=[self.category.slug,self.slug])
 
 
+class Cart(models.Model):
+    cart_id = models.CharField(max_length=250,blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.cart_id
+    
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.product 
